c9b58b1b5577b87b17bf2c1511308fb277a17aea
Dwayne <dune.njenga@student.moringaschool.com>
2025-06-28 23:24:33 +0300

    Added project data fetch request

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,15 @@ import { useEffect, useState } from "react"
 
 function App() { 
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5555/projects')
+      .then((res) => res.json())
+      .then((data) => setProjects(data))
+      .catch((err) => console.error('Error fetching projects:', err));
+  }, []);
+
 
   return (
     <>
